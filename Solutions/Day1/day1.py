26b48865b5b37fb628b01cb0ca2c3dc59fd1006d
Erik Oudsen <e.oudsen@ncim.nl>
2022-12-01 20:01:06 +0100

    DAY-1: Add solution for part 1 and part 2 with input. Prepare for day 2.

--- Solutions/Day1/day1.py
+++ Solutions/Day1/day1.py
@@ -1,16 +1,40 @@
 
 def retrieve_input():
-    return [x for x in open('input.txt').read().strip('\n')]
+    return [line.strip() for line in open('input.txt')]
 
 
 def solve_part1():
     lines = retrieve_input()
+    max_calories = 0
+    current_elf_calories = 0
+    for line in lines:
+        if len(line) == 0:
+            if current_elf_calories > max_calories:
+                max_calories = current_elf_calories
+            current_elf_calories = 0
+            continue
+        current_elf_calories += int(line)
+    if current_elf_calories > max_calories:
+        max_calories = current_elf_calories
+    return max_calories
 
 
 def solve_part2():
     lines = retrieve_input()
+    elf_calories = []
+    current_elf_calories = 0
+    for line in lines:
+        if len(line) == 0:
+            elf_calories.append(current_elf_calories)
+            current_elf_calories = 0
+            continue
+        current_elf_calories += int(line)
+    elf_calories.append(current_elf_calories)
+    sorted_elf_categories = sorted(elf_calories)
+    sorted_elf_categories.reverse()
+    return sum(sorted_elf_categories[:3])
 
 
 if __name__ == "__main__":
-    print(solve_part1())
-    print(solve_part2())
+    print(solve_part1())  # 69528
+    print(solve_part2())  # 206152
