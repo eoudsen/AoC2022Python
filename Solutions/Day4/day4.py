31c3b3d11e210dcab9da0ee7118641c706b0fece
Erik Oudsen <e.oudsen@ncim.nl>
2022-12-04 08:38:32 +0100

    DAY-4: Add solution for part 1 and part 2 with input. Prepare for day 5.

--- Solutions/Day4/day4.py
+++ Solutions/Day4/day4.py
@@ -4,10 +4,28 @@ def retrieve_input():
 
 def solve_part1():
     lines = retrieve_input()
+    total_inside = 0
+    for line in lines:
+        left_elf = line.split(",")[0]
+        right_elf = line.split(",")[1]
+        if (int(left_elf.split("-")[0]) <= int(right_elf.split("-")[0]) and int(left_elf.split("-")[1]) >= int(right_elf.split("-")[1])) or (int(left_elf.split("-")[0]) >= int(right_elf.split("-")[0]) and int(left_elf.split("-")[1]) <= int(right_elf.split("-")[1])):
+            total_inside += 1
+    return total_inside
 
 
 def solve_part2():
     lines = retrieve_input()
+    total_overlap = 0
+    for line in lines:
+        left_elf = line.split(",")[0]
+        right_elf = line.split(",")[1]
+        if (int(left_elf.split("-")[0]) <= int(right_elf.split("-")[0]) and int(left_elf.split("-")[1]) >= int(right_elf.split("-")[1])) or (int(left_elf.split("-")[0]) >= int(right_elf.split("-")[0]) and int(left_elf.split("-")[1]) <= int(right_elf.split("-")[1])):
+            total_overlap += 1
+        elif int(right_elf.split("-")[0]) <= int(left_elf.split("-")[0]) <= int(right_elf.split("-")[1]):
+            total_overlap += 1
+        elif int(left_elf.split("-")[0]) <= int(right_elf.split("-")[0]) <= int(left_elf.split("-")[1]):
+            total_overlap += 1
+    return total_overlap
 
 
 if __name__ == "__main__":
