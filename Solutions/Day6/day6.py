4ce8660f5be23ec5fce96ed8dca8b61ffbf2b141
Erik Oudsen <e.oudsen@gmail.com>
2022-12-06 18:50:56 +0100

    DAY-6: Add result of day 6, and prepare for day 7.

--- Solutions/Day6/day6.py
+++ Solutions/Day6/day6.py
@@ -1,15 +1,25 @@
+from itertools import islice
+
+
 def retrieve_input():
     return [x.strip() for x in open('input.txt')]
 
 
+def rolling_window(input_list, window_size):
+    for i in range(len(input_list) - window_size + 1):
+        yield input_list[i: i + window_size]
+
+
 def solve_part1():
-    lines = retrieve_input()
+    line = retrieve_input()[0]
+    return [len(set(x)) for x in rolling_window(list(line), 4)].index(4) + 4
 
 
 def solve_part2():
-    lines = retrieve_input()
+    line = retrieve_input()[0]
+    return [len(set(x)) for x in rolling_window(list(line), 14)].index(14) + 14
 
 
 if __name__ == "__main__":
-    print(solve_part1())
-    print(solve_part2())
+    print(solve_part1())  # 1816
+    print(solve_part2())  # 2625
